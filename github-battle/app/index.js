eb8f340767cb766fb8e4dc3850a1f2472bbe5e4f
Danilo-Zekovic <danilozeka93@gmail.com>
2017-03-22 21:45:44 +0100

    Started new lesson

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,5 +1,21 @@
+var USER_DATA = {
+  name: 'Tyler McGinnis',
+  username: 'tylermcginnis',
+  image: 'https://avatars0.githubusercontent.com/u/2933430?v=3$s=460'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+var ProfilePic = React.createClass({
+  render: function(){
+    return (
+      <img src={this.props.imageUrl} style={{height:100, width:100}}>
+    )
+  }
+})
+
+}
 
 var HelloWorld = React.createClass({
   render: function(){
