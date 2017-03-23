9efde8729cf3d76a558c95ed24c8f2f0ea1b3334
Danilo-Zekovic <danilozeka93@gmail.com>
2017-03-23 15:34:18 +0100

    Done with this lesson

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -8,24 +8,40 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var ProfilePic = React.createClass({
-  render: function(){
-    return (
-      <img src={this.props.imageUrl} style={{height:100, width:100}}>
-    )
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}></img>
   }
-})
+});
 
-}
-
-var HelloWorld = React.createClass({
-  render: function(){
+var ProfileLink = React.createClass({
+  render: function () {
     return (
-      <div> Hello World </div>
-    )
+      <div>
+        <a href={'https://www.github.com/' +  this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
   }
-})
+});
 
-ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById('app')
-)
+var ProfileName = React.createClass({
+  render: function (){
+    return <div>{this.props.name}</div>
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return(
+      <div>
+        <ProfilePic imageUrl={this.props.user.image}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
