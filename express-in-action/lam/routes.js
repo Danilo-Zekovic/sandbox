d9416a3229f49b18e07923ea9326ac85bac39202
Danilo Zekovic <danilozeka93@gmail.com>
2017-06-01 10:15:13 +0200

    all the views

--- express-in-action/lam/routes.js
+++ express-in-action/lam/routes.js
@@ -6,6 +6,16 @@ var router = express.Router()
 
 router.use(function(req, res, next){
   res.locals.currentUser = req.User
-  res.locals.errors = 
-  res.locals.
+  res.locals.errors = req.flash("error")
+  res.locals.infos = req.flash("info")
+  next()
+})
+
+router.get("/", function(req, res, next){
+  User.find()
+  .sort({createdAt:"descending"})
+  .exec(function(err, users){
+    if(err) {return next(err)}
+    res.render("index", {users:users})
+  })
 })
