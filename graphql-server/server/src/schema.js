e0e24f76d68949b01411ac1fdedf1315aaf00418
Danilo Zekovic <danilozeka93@gmail.com>
2017-06-17 17:26:46 +0200

    done with part 2

---
+++ graphql-server/server/src/schema.js
@@ -0,0 +1,19 @@
+// src/schema.js
+import {
+  makeExecutableSchema,
+  addMockFunctionsToSchema,
+} from 'graphql-tools';
+import { resolvers } from './resolvers';
+const typeDefs = `
+  type Channel {
+     id: ID!
+     name: String
+  }
+
+  type Query {
+     channels: [Channel]    # "[]" means this is a list of channels
+  }
+`;
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+//addMockFunctionsToSchema({ schema });
+export { schema };
